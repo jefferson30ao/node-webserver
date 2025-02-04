0ec2be35c88f9816277d024fc91e901aaee71009
jefferson30 <jefferson30.ao@gmail.com>
2025-02-04 00:58:08 -0500

    Salidas agregadas

--- app.js
+++ app.js
@@ -15,24 +15,31 @@ res.send('Hola Mundo en su respectiva ruta')
 
 app.get('/', (req,res) => {
   res.render('home', { // estos son los argumentos
-    nombre: 'Armando David',
+    nombre: 'Armando David Espinoza',
     titulo: 'Curso de Node'
   });
 })
 
 app.get('/elements', (req, res) => {
     res.render('elements', {
-      nombre: 'Armando David',
+      nombre: 'Armando David Espinoza',
       titulo: 'Curso de Node'
     })
   });
 
   app.get('/generic', (req, res) => {
     res.render('generic', {
-      nombre: 'Armando David',
+      nombre: 'Armando David Espinoza',
       titulo: 'Curso de Node'
     })
   });
+
+  app.get('/salidas', (req, res) => {
+    res.render('salidas', {
+      nombre: 'Armando David Espinoza',
+      titulo: 'Curso de Node'
+    })
+ });
 
 app.get('*', (req, res)=> {
     res.sendFile(__dirname +'/public/404.html');
